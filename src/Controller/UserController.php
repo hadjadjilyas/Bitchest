32b1b9a175a33d445fed850d546bded9d1163519
Aimé Ngouinda <juniorngouinda@gmail.com>
2024-02-17 22:22:47 +0100

    suppression des migrations

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-#[Route('/user')]
+#[Route('/')]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
